eed57278fc04380daf93763591b9d30670400833
Ferran Pons <ferranpons@gmail.com>
2024-07-16 19:59:44 +0200

    removed unneeded files

--- androidApp/src/main/java/com/multiplatformkickstarter/app/android/MainActivityUiState.kt
+++
@@ -1,8 +0,0 @@
-package com.multiplatformkickstarter.app.android
-
-import com.multiplatformkickstarter.app.common.model.UserData
-
-sealed interface MainActivityUiState {
-    data object Loading : MainActivityUiState
-    data class Success(val userData: UserData) : MainActivityUiState
-}
