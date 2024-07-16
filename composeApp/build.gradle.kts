33bf3a452238d6493eccf4c67446eb5f82c8ca9f
Ferran Pons <ferranpons@gmail.com>
2024-07-16 19:58:42 +0200

    added missing desktop dependencies

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -72,6 +72,49 @@ kotlin {
         }
         desktopMain.dependencies {
             implementation(compose.desktop.currentOs)
+            implementation(libs.skiko)
+            implementation(compose.ui)
+            implementation(compose.foundation)
+            implementation(compose.material)
+            implementation(compose.material3)
+            implementation(compose.animation)
+            implementation(compose.materialIconsExtended)
+            @OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class)
+            implementation(compose.components.resources)
+            implementation(libs.skiko.macos.arm64)
+
+            implementation(libs.androidx.compose.ui.util)
+
+            implementation(libs.kotlinx.serialization.json)
+            implementation(libs.kotlinx.datetime)
+            implementation(libs.kotlinx.coroutines.core)
+            implementation(libs.kotlinx.coroutines.swingui)
+            implementation(libs.ktor.client)
+            implementation(libs.ktor.client.java)
+            implementation(libs.ktor.client.content.negotiation)
+            implementation(libs.ktor.server.serialization.kotlinx.json)
+
+            // Kamel for image loading
+            implementation(libs.kamel)
+
+            // Voyager for Navigation
+            implementation(libs.voyager.navigator)
+            implementation(libs.voyager.bottom.sheet.navigator)
+            implementation(libs.voyager.tab.navigator)
+            implementation(libs.voyager.transitions)
+            implementation(libs.voyager.koin)
+
+            // Multiplatform Settings to encrypted key-value data
+            implementation(libs.multiplatform.settings.no.arg)
+            implementation(libs.multiplatform.settings.serialization)
+
+            // Dependency Injection
+            implementation(libs.koin.core)
+            implementation(libs.koin.test)
+            implementation(libs.koin.compose)
+
+            // Logging
+            implementation(libs.kermit)
         }
     }
 }
