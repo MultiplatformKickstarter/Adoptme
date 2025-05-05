010e8b9c3e80b4d5ce6afe7d0b5a49d9a60ff43a
Ferran Pons <ferranpons@gmail.com>
2025-05-05 13:25:03 +0200

    upgraded project to latest versions

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -41,7 +41,7 @@ kotlin {
     androidTarget {
         @OptIn(ExperimentalKotlinGradlePluginApi::class)
         compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_17)
+            jvmTarget.set(JvmTarget.JVM_21)
         }
     }
     jvm("desktop")
@@ -151,8 +151,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
     buildFeatures {
         compose = true
