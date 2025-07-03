cb16b653dfdbb8a7e32ccacbcfc2cad2da8eb220
Ferran Pons <ferranpons@gmail.com>
2025-07-03 15:44:21 +0200

    corrected Ktlint issue

--- composeApp/src/iosMain/kotlin/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/MainViewController.kt
@@ -2,8 +2,7 @@ import androidx.compose.ui.window.ComposeUIViewController
 import com.multiplatformkickstarter.app.MainApp
 import platform.UIKit.UIViewController
 
-fun homeScreenViewController(): UIViewController = ComposeUIViewController {
-    MainApp()
-}
-
-/* ktlint-disable */
+fun homeScreenViewController(): UIViewController =
+    ComposeUIViewController {
+        MainApp()
+    }
