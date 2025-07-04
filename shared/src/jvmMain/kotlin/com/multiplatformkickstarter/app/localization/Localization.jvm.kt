7c4d71fb55dd4090516f13dd8269aafb73c604f1
Ferran Pons <ferranpons@gmail.com>
2025-07-04 09:50:48 +0200

    corrected Ktlint issue

--- shared/src/jvmMain/kotlin/com/multiplatformkickstarter/app/localization/Localization.jvm.kt
+++ shared/src/jvmMain/kotlin/com/multiplatformkickstarter/app/localization/Localization.jvm.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 
 actual fun getCurrentLanguage(): AvailableLanguages = AvailableLanguages.EN
 
+@Suppress("FunctionName")
 @Composable
 actual fun SetLanguage(language: AvailableLanguages) {
 }
--- shared/src/jvmMain/kotlin/com/multiplatformkickstarter/app/platform/Resources.jvm.kt
+++ shared/src/jvmMain/kotlin/com/multiplatformkickstarter/app/platform/Resources.jvm.kt
@@ -12,11 +12,17 @@ private val cache: MutableMap<String, Font> = mutableMapOf()
 
 @OptIn(InternalResourceApi::class)
 @Composable
-actual fun font(name: String, res: String, weight: FontWeight, style: FontStyle): Font {
+actual fun font(
+    name: String,
+    res: String,
+    weight: FontWeight,
+    style: FontStyle,
+): Font {
     return cache.getOrPut(res) {
-        val byteArray = runBlocking {
-            readResourceBytes("font/$res.ttf")
-        }
+        val byteArray =
+            runBlocking {
+                readResourceBytes("font/$res.ttf")
+            }
         androidx.compose.ui.text.platform.Font(res, byteArray, weight, style)
     }
 }
