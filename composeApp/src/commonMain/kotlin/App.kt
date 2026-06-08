83c4dc6adab4d43ddf61b45580243f6168741be2
Ferran Pons <ferranpons@gmail.com>
2026-06-08 16:27:41 +0200

    Removed preview for app

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,10 +1,8 @@
 import androidx.compose.runtime.Composable
 import com.multiplatformkickstarter.app.MainApp
-import androidx.compose.ui.tooling.preview.Preview
 
 @Suppress("FunctionName")
 @Composable
-@Preview
 fun App() {
     MainApp()
 }
